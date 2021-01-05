cc4463d82c51a5f7ec1f0c5be01a090524b7cbfb
ChrisOfNormandy <boobrick@hotmail.com>
2021-01-05 12:19:01 -0500

    Removed test items; Added concrete + large tiles + bricks and its variants + products. No recipe.

---
+++ runClient.bat
@@ -0,0 +1,5 @@
+cd mc-mod-json-builder
+node main.js
+
+cd ..
+gradlew runClient
--- src/main/java/com/conmod/redemption/ModBlocks.java
+++ src/main/java/com/conmod/redemption/ModBlocks.java
@@ -10,8 +10,9 @@ import net.minecraft.block.Block;
 
 public class ModBlocks {
     public static void Init() {
-        Groups itemGroup = ModRegister.itemGroup("redemption_blocks", "testblock");
+        Groups itemGroup = ModRegister.itemGroup("redemption_blocks", "concrete");
 
+        /*
         Main.registerRockSuite("testblock", 1, itemGroup);
         Main.registerWoodSuite("testwood", itemGroup);
 
@@ -22,5 +23,17 @@ public class ModBlocks {
         Main.registerNode("richtestnode", nodedrop, Tier.RICH, itemGroup);
 
         ModRegister.registerOre("testore", new OreBase(1, 4, 2), itemGroup);
+        */
+
+        Main.registerRockSuite("concrete_bricks", 2, itemGroup);
+        Main.registerRockSuite("concrete_large_tile", 2, itemGroup);
+        Main.registerRockSuite("concrete", 2, itemGroup);
+        Main.registerRockSuite("cracked_concrete_bricks", 2, itemGroup);
+        Main.registerRockSuite("cracked_concrete_large_tile", 2, itemGroup);
+        Main.registerRockSuite("cracked_concrete", 2, itemGroup);
+        Main.registerRockSuite("mossy_concrete_bricks", 2, itemGroup);
+        Main.registerRockSuite("mossy_concrete_large_tile", 2, itemGroup);
+        Main.registerRockSuite("mossy_concrete", 2, itemGroup);
+        Main.registerRockSuite("polished_concrete", 2, itemGroup);
     }
 }
