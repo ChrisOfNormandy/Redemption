2fb1bffe09319f4b8b39bbf6b930b10539eed7a7
ChrisOfNormandy <boobrick@hotmail.com>
2021-01-05 22:10:38 -0500

    Added dyed clay, brick items; Added some recipes.

--- src/main/java/com/conmod/redemption/ModItems.java
+++ src/main/java/com/conmod/redemption/ModItems.java
@@ -11,5 +11,8 @@ public class ModItems {
     public static void Init() {
         Item icon = Main.create("items_icon", new Properties());
         Groups itemGroup = ModRegister.itemGroup("redemption_items", icon);
+
+        Main.registerDyedSuite("clay_ball", new Properties(), itemGroup);
+        Main.registerDyedSuite("brick", new Properties(), itemGroup);
     }
 }
