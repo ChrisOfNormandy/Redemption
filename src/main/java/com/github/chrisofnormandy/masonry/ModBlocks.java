3eaa8fd6b155e34cf3082e4c5862db9df451b111
ChrisOfNormandy <boobrick@hotmail.com>
2021-01-07 22:31:54 -0500

    Changed name to masonry; Fixed lang file.

---
+++ src/main/java/com/github/chrisofnormandy/masonry/ModBlocks.java
@@ -0,0 +1,37 @@
+package com.github.chrisofnormandy.masonry;
+
+import com.github.chrisofnormandy.conlib.block.Main;
+import com.github.chrisofnormandy.conlib.block.OreBase;
+import com.github.chrisofnormandy.conlib.block.Node.Tier;
+import com.github.chrisofnormandy.conlib.itemgroup.Groups;
+import com.github.chrisofnormandy.conlib.registry.ModRegister;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+
+public class ModBlocks {
+    public static void Init() {
+        Block parent = Main.create_rock(1);
+        Groups blocks = ModRegister.itemGroup("masonry_blocks", ModRegister.registerBlock("icon_block", parent));
+        Groups slabs = ModRegister.itemGroup("masonry_slabs", ModRegister.registerBlock("icon_slab", Main.create_slab(parent)));
+        Groups stairs = ModRegister.itemGroup("masonry_stairs", ModRegister.registerBlock("icon_stairs", Main.create_stairs(parent)));
+        Groups walls = ModRegister.itemGroup("masonry_walls", ModRegister.registerBlock("icon_wall", Main.create_wall(parent)));
+        Groups[] groups = new Groups[] {blocks, slabs, stairs, walls};
+
+        Main.registerRockSuite_withProducts("concrete", 2, groups, true);
+        Main.registerRockSuite("concrete_large_tile", 2, groups);
+        Main.registerRockSuite("cracked_concrete_large_tile", 2, groups);
+        Main.registerRockSuite("mossy_concrete_large_tile", 2, groups);
+
+        Main.registerRockSuite_withProducts("polished_concrete", 2, groups, true);
+        Main.registerRockSuite("polished_concrete_large_tile", 2, groups);
+        Main.registerRockSuite("cracked_polished_concrete_large_tile", 2, groups);
+        Main.registerRockSuite("mossy_polished_concrete_large_tile", 2, groups);
+
+        for (int i = 0; i < Main.dyes.length; i++) {
+            Main.registerBrickSuite(Main.dyes[i], groups);
+        }
+
+        Main.registerDyedSuite("clay", Blocks.CLAY, blocks);
+    }
+}
---
+++ src/main/java/com/github/chrisofnormandy/masonry/ModItems.java
@@ -0,0 +1,18 @@
+package com.github.chrisofnormandy.masonry;
+
+import com.github.chrisofnormandy.conlib.item.Main;
+import com.github.chrisofnormandy.conlib.itemgroup.Groups;
+import com.github.chrisofnormandy.conlib.registry.ModRegister;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.Properties;
+
+public class ModItems {
+    public static void Init() {
+        Item icon = Main.create("items_icon", new Properties());
+        Groups itemGroup = ModRegister.itemGroup("masonry_items", icon);
+
+        Main.registerDyedSuite("clay_ball", new Properties(), itemGroup);
+        Main.registerDyedSuite("brick", new Properties(), itemGroup);
+    }
+}
