ec4aaac252bb14fd794e83e50bed5edbe325d39b
ChrisOfNormandy <boobrick@hotmail.com>
2021-03-05 17:51:20 -0500

    Updated to 1.16.5

---
+++ run.bat
@@ -0,0 +1,8 @@
+mvn install:install-file ^
+-DgroupId=com.github.chrisofnormandy ^
+-DartifactId=masonry ^
+-Dversion=1.0 ^
+-Dfile=build/libs/masonry-1.0.jar ^
+-Dpackaging=jar ^
+-DlocalRepositoryPath=. ^
+-DcreateChecksum=true
